515f4b92469ec65e687aee5ba7b5752580ef7e23
mark-thompson01 <Markandrewt01@protonmail.com>
2026-02-06 19:16:54 -0700

    Add files via upload

---
+++ Current Projects & Studies/Hyper-V Lab Extended/System Setup/Images/Run-DiskCleanup.ps1
@@ -0,0 +1,10 @@
+﻿# Run-DiskCleanup.ps1
+# This script configures and runs Disk cleanup silently based on preset options
+
+Write-Host "Setting up Disk Cleanup options..." -ForegroundColor Cyan
+Start-Process -FilePath "cleanmgr.exe" -ArgumentList "/sageset:1" -Verb RunAs -Wait
+
+Write-Host "Running Disk Cleanup..." -ForegroundColor Cyan
+Start-Process -FilePath "cleanmgr.exe" -ArgumentList "/sagerun:1" -Verb RunAs -Wait
+
+Write-Host "'nDisk Cleanup complete." -ForegroundColor Green
